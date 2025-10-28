51cfaac5bbf844a830ed7d742e02bf4f87eff837
Betremariam <yihenewbmariam@gmail.com>
2025-10-28 10:46:19 -0500

    register screen created

--- frontend/lib/screens/register_screen.dart
+++ frontend/lib/screens/register_screen.dart
@@ -1,1 +1,67 @@
+import 'package:flutter/material.dart';
+import '../services/api_service.dart';
+import '../models/user_model.dart';
+import 'notes_screen.dart';
 
+class RegisterScreen extends StatefulWidget {
+  const RegisterScreen({super.key});
+
+  @override
+  State<RegisterScreen> createState() => _RegisterScreenState();
+}
+
+class _RegisterScreenState extends State<RegisterScreen> {
+  final _nameController = TextEditingController();
+  final _emailController = TextEditingController();
+  bool _loading = false;
+
+  Future<void> _registerUser() async {
+    setState(() => _loading = true);
+    User? user = await ApiService.createUser(
+      _nameController.text,
+      _emailController.text,
+    );
+    setState(() => _loading = false);
+
+    if (user != null) {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (_) => NotesScreen(user: user)),
+      );
+    } else {
+      ScaffoldMessenger.of(
+        context,
+      ).showSnackBar(const SnackBar(content: Text('Registration failed')));
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Register')),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            TextField(
+              controller: _nameController,
+              decoration: const InputDecoration(labelText: 'Name'),
+            ),
+            TextField(
+              controller: _emailController,
+              decoration: const InputDecoration(labelText: 'Email'),
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: _loading ? null : _registerUser,
+              child:
+                  _loading
+                      ? const CircularProgressIndicator()
+                      : const Text('Register'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
